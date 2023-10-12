2e6ec6459abe0e174c4181815698273d10252466
gui couto <gcoutos95@gmail.com>
2023-10-12 06:56:24 -0300

    feat: exercise 3

--- myApp.js
+++ myApp.js
@@ -6,8 +6,15 @@ ex. 1
 console.log('Hello World');
 */ 
 
+/* 
+ex.2
 app.get('/', function(req, res) {
   res.send('Hello Express');
+});
+*/
+
+app.get('/', function(req, res) {
+  res.sendFile('/views/index.html', { root: __dirname });
 });
 
 module.exports = app;
