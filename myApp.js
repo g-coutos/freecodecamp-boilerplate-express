801cec9463d3a08f2fde47b6fdf3c4a5d272769d
gui couto <gcoutos95@gmail.com>
2023-10-12 06:34:07 -0300

    feat: exercise 2

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 let express = require('express');
 let app = express();
 
+/*
+ex. 1
 console.log('Hello World');
+*/ 
+
+app.get('/', function(req, res) {
+  res.send('Response String');
+});
 
 module.exports = app;
