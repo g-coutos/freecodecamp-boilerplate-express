5a7d5f6fd7ce200eb40557a15ab46332830e83ae
gui couto <gcoutos95@gmail.com>
2023-10-12 07:24:31 -0300

    feat: exercise 4

--- myApp.js
+++ myApp.js
@@ -13,8 +13,17 @@ app.get('/', function(req, res) {
 });
 */
 
+/* 
+ex. 3
 app.get('/', function(req, res) {
   res.sendFile('/views/index.html', { root: __dirname });
 });
+*/
+
+app.get('/public', function(req, res) {
+  res.sendFile('/views/index.html', { root: __dirname });
+});
+
+app.use('/public', express.static(`${__dirname}/public`));
 
 module.exports = app;
